a736a16a96a737967d8524e2d341b42cde006c55
Rishi <rishi@demo.com>
2018-09-25 06:01:51 +0800

    Delete project

--- CustomerService.module/Tests/A3DEWS2RF4.ml
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="ASCII"?>
-<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
-  <CurrentLoadedTest href="platform:/resource/CustomerService.module/Tests/CustomerDBTest.bwt#/"/>
-  <EmulationDataFile>ACED0005740012437573746F6D65724442546573742E627774</EmulationDataFile>
-</emulation:BWTFile>
