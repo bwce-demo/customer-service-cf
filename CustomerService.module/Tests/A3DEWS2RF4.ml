64f1f9c23a1f15c36ff88037a08ad97705b70b6a
Rishi <rishi@demo.com>
2018-09-24 21:28:01 +0530

    Unit test

--- CustomerService.module/Tests/A3DEWS2RF4.ml
+++ CustomerService.module/Tests/A3DEWS2RF4.ml
@@ -2,4 +2,5 @@
 <emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
   <CurrentLoadedTest href="platform:/resource/CustomerService.module/Tests/CustomerDetailsTest.bwt#/"/>
   <EmulationDataFile>ACED0005740017437573746F6D657244657461696C73546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001754657374437573746F6D657244657461696C732E627774</EmulationDataFile>
 </emulation:BWTFile>
